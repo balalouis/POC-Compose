92dbebdc6689736c71f812fc45806ff637e1f7ae
SBALAMU1 <balan.ssn@gmail.com>
2022-09-15 07:49:55 +0530

    Learned modifiers, styles

--- app/src/main/java/com/clean/poc/poc_compose/MainActivity.kt
+++ app/src/main/java/com/clean/poc/poc_compose/MainActivity.kt
@@ -1,28 +1,50 @@
 package com.clean.poc.poc_compose
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.clean.poc.poc_compose.ui.theme.POCComposeTheme
-import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val personList: List<Person> = mutableListOf(
+            Person("Arun", "Salem"),
+            Person("Anand", "Salem"),
+            Person("Banana", "France"),
+            Person("Chitra", "Chennai")
+        )
+
         setContent {
-            DisplayText(Person("Arun kumar", "Salem"))
+//            DisplayText(Person("Arun kumar", "Salem"))
+            conversation(personList)
+        }
+    }
+}
+
+@Composable
+fun conversation(listPerson: List<Person>) {
+    LazyColumn {
+        items(listPerson) { person ->
+            DisplayText(person)
         }
     }
 }
@@ -31,6 +53,10 @@ data class Person(val name: String, val location: String)
 
 @Composable
 fun DisplayText(person: Person) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
@@ -38,12 +64,30 @@ fun DisplayText(person: Person) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(4.dp))
         Column {
-            Text(text = "Hello ${person.name}")
+            Text(
+                text = "Hello ${person.name}",
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.body2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = person.location)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.5.dp) {
+                Text(
+                    text = person.location,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+        Button(
+            onClick = { "Hello World" },
+            interactionSource = interactionSource,
+            modifier = Modifier.padding(all = 4.dp)
+        ) {
+            Text(if (isPressed) "Pressed!" else "Not pressed")
         }
     }
 }
